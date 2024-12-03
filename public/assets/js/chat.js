85464e70110a7c1623e5a24caf6ae1ef0f9403d6
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-03 13:09:19 -0500

    Update chat.js

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,6 +1,6 @@
 var chatframe = document.getElementById("chatframe");
 var url;
 
-localStorage.setItem("/static/iframe.html#" + "https://e.widgetbot.io/channels/1297323622024609874/1307106191821897831", url);
+localStorage.setItem(url, "/static/iframe.html#" + "https://e.widgetbot.io/channels/1297323622024609874/1307106191821897831");
 
 chatframe.setAttribute("src", "/browser.html");
