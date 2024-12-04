3d9e5ddd12739a0067209b9e32ee362f33707bca
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-04 09:56:30 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,9 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/uv/search/',
+    bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
