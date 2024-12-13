1b751f7bac422fc83fe99bfb266a8ee5cce7eeef
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-13 10:57:11 -0500

    Add files via upload

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,10 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/uv/service/',
-    bare: 'https://bare.benrogo.net',
+    prefix: '/static/tiw',
+    bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
 };
